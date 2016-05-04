45cd76ef15d2e0f1fb6218ad519948af7701b8ac
LiuchangDuan <870121425@qq.com>
2016-05-04 12:05:14 +0800

    自定义View的属性

--- app/src/main/java/com/example/customview/view/CustomTitleView.java
+++ app/src/main/java/com/example/customview/view/CustomTitleView.java
@@ -1,7 +1,22 @@
 package com.example.customview.view;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by Administrator on 2016/5/4.
  */
-public class CustomTitleView {
+public class CustomTitleView extends View {
+    public CustomTitleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomTitleView(Context context) {
+        super(context);
+    }
+
+    public CustomTitleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 }
